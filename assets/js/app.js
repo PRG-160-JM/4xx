35926bb05ca2818da0cd4316d4f4b71ddd221b7c
jonathanmiller <penguinsj71@comcast.net>
2018-04-29 22:29:50 -0400

    we added menu items to complete ajax calls to pull posts and pages from our WP me site.

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,7 +3,8 @@
 
 var appData = {
 	title: 'inside out - js',
-	tagLine: 'continuously falling forward into the light...'
+	tagLine: 'continuously falling forward into the light...',
+	sideMenu: ['posts', 'pages']
 };
 
 window.addEventListener('load', initializeApplication);
@@ -58,7 +59,7 @@ function displayPB() {
 		document.getElementById('loaderProgressBar').setAttribute('aria-valuenow', timerCount);
 		document.getElementById('loaderProgressBar').style.width = timerCount + '%    ';
 		timerCount++;
-		setTimeout(displayPB, 50);
+		setTimeout(displayPB, 15);
 	} else {
 		timerCount = 0;
 
@@ -98,17 +99,15 @@ function applicationUserInterface() {
 }
 
 function buildMenu() {
-	
+
 	let sm = '<nav class="sidebar animated slideInLeft"><ul class="nav flex-column">';
-	
-	//return '<li class="nav-item"> <a class="nav-link active" data-dest="link01" href="#">Active</a> </li>';
-	
-		for (let i = 0; i < quotArr.length; i++) {
-			sm += '<li class="nav-item"><a class="nav-link active" data-dest="' + i + '" href="#">' + quotArr[i][1].split(",",1) + '</a></li>';
-		}
-	
+
+	for (let i = 0; i < appData.sideMenu.length; i++) {
+		sm += '<li class="nav-item"><a class="nav-link active" data-dest="' + appData.sideMenu[i] + '" href="#">' + appData.sideMenu[i] + '</a></li>';
+	}
+
 	sm += '</ul></nav>';
-	
+
 	return sm;
 }
 
@@ -118,8 +117,13 @@ function buildMain() {
 
 function linkClicked(obj) {
 	console.log(obj.dataset.dest);
-	
-	 var str = '<div class="infoDiv"><h1 class="animated zoomIn">' + quotArr[obj.dataset.dest][0] + '</h1><div class="animated slideInRight auth">- ' + quotArr[obj.dataset.dest][1] + '</div></div>';
-	
-	document.getElementById("main").innerHTML = str;
+
+	if (appData.sideMenu.includes(obj.dataset.dest)) {
+		ajax.get('https://me.beardednbroke.com/wp-json/wp/v2/' + obj.dataset.dest, {
+			per_page: '50'
+		}, useData);
+	} else {
+		var str = '<div class="infoDiv"><h1 class="animated zoomIn">' + quotArr[obj.dataset.dest][0] + '</h1><div class="animated slideInRight auth">- ' + quotArr[obj.dataset.dest][1] + '</div></div>';
+		document.getElementById("main").innerHTML = str;
+	}
 }
